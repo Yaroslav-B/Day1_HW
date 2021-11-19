5baaf70724362f0019517babf0b99d3bdf4ef4ed
Yaroslav-B <sync2galaxy@gmail.com>
2021-11-19 12:30:06 +0200

    Added functions for localStorage - save and read from localStorage all products in the store; - functions for adding new products from adding form; - auto-update list of products on main page

--- src/index.js
+++ src/index.js
@@ -22,10 +22,63 @@ const App = {
             count = prods.length;
         }
         elem.textContent = count;
-    }
+    },
+
+    updateListOfProductsOnMainPage: function(){
+        let allProducts = this.getAllProductsInStore()
+        let length = allProducts.length;
+        if(length < 1)
+        {
+            return;
+        }
+
+        for (let i = 0; i < length; i++) {
+            const product = allProducts[i];
+            
+            let elemProd = document.createElement("div");
+            elemProd.className = "main__items__item";
+
+            let elemTit = document.createElement("div");
+            elemTit.className = "main__items__item--tittle";
+            elemTit.textContent = product.tittle; 
+
+            let elemImg = document.createElement("img");
+            elemImg.className = "main__items__item--image";
+            elemImg.src = product.image; 
+            elemImg.alt = "image";
+
+            let elemDesc = document.createElement("div");
+            elemDesc.className = "main__items__item--description";
+            elemDesc.textContent = product.description;
+
+            let elemBut = document.createElement("input");
+            elemBut.type = "button";
+            elemBut.className = "main__items__item--button";
+            elemBut.value = "Add to basket";
+
+            elemProd.appendChild(elemTit);
+            elemProd.appendChild(elemImg);
+            elemProd.appendChild(elemDesc);
+            elemProd.appendChild(elemBut);
+            
+            let mainItems = document.querySelector(".main__items");
+            mainItems.appendChild(elemProd);                
+        }   
+    },
+
+    getAllProductsInStore: () => {
+        let products = JSON.parse(localStorage.getItem("products"));
+        if(products === null)
+        {
+            products = [];
+        }
+        return products;
+    },
+
+    
 }
 
 
 App.updateBacketCountIcon();
-
+App.updateListOfProductsOnMainPage();
 console.log("Index.js is loaded.")
