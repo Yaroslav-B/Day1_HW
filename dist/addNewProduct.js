15cdbd5e9c598deed22239a245121257ff42a4dd
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-11-22 16:39:36 +0200

    Webpack conf. - renamed files, changed pathes

---
+++ dist/addNewProduct.js
@@ -0,0 +1,60 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+console.log("addingNewProduct.js is loading...");
+
+const AddingNewProductToStore = {
+    
+    addEvents: function(){
+        let elemAdd = document.querySelector(".btn-block_add"); 
+        elemAdd.addEventListener("click", this.addNewProduct);     
+        
+        let elemRes = document.querySelector(".btn-block_resset");
+        elemRes.addEventListener("click", this.resetForm);
+    },
+    
+    addNewProduct: function(e){
+        let form = document.addingNewProductForm;
+        let product = {};
+        let products = [];
+        product.tittle = form.tittle.value;
+        product.description = form.description.value;
+        product.image = form.ImageUrl.value;
+        product.category = form.category.value;
+
+        if(product.tittle == "" || product.description == "")
+        {
+            // e.preventDefault()
+            return;
+        }
+        if(product.image == "")
+        {
+            product.image = "Service/noImage.png";
+        }
+
+        let storedPproducts = JSON.parse(localStorage.getItem("products"));
+        if(storedPproducts !== null)
+        {
+            products = storedPproducts;        
+        }
+        let index = products.length;
+        products[index] = product;
+        localStorage.setItem("products", JSON.stringify(products));
+
+        console.log("Added new product: \n" + form.tittle.value +"\n" + form.description.value);
+    },
+
+    resetForm: function(){
+        //tbd
+    }
+    
+    
+}
+
+AddingNewProductToStore.addEvents();
+
+console.log("addingNewProduct.js is loaded.");
+/******/ })()
+;
---
+++ dist/basket.js
@@ -0,0 +1,57 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+const Basket = {
+    length: 0,
+
+    UpdateBasketPage: () => {
+        let products = Basket.getAddedProducts();
+
+        if(length > 0)
+        {
+            for (let i = 0; i < length; i++) {
+                const product = products[i];
+                
+                let elemProd = document.createElement("div");
+                elemProd.className = "main__items__product";
+
+                let elemTit = document.createElement("div");
+                elemTit.className = "main__items__product--tittle";
+                elemTit.textContent = product.tittle; 
+
+                let elemDesc = document.createElement("div");
+                elemDesc.className = "main__items__product--description";
+                elemDesc.textContent = product.description;
+
+                let elemSt = document.createElement("div");
+                elemSt.className = "main__items__product--status";
+                elemSt.textContent = "new!"
+
+                elemProd.appendChild(elemTit);
+                elemProd.appendChild(elemDesc);
+                elemProd.appendChild(elemSt);
+                
+                let mainItems = document.querySelector(".basket__items");
+                mainItems.appendChild(elemProd);                
+            }           
+        }
+    },
+
+    getAddedProducts: () => {
+        let products = JSON.parse(sessionStorage.getItem("products"));
+        if(products === null)
+        {
+            products = [];
+        }
+        length = products.length;
+        return products;
+    }  
+}
+
+Basket.UpdateBasketPage();
+App.UpdateBasketCountIcon();
+
+/******/ })()
+;
